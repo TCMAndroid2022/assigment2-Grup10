24ccaff3299c1ab897cfc24f6112fead64100a7d
Maricel <59828070+Mar-i-cel@users.noreply.github.com>
2022-05-24 19:26:29 +0200

    commit 5

---
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/LluisBoschNadal/IvanCarolNavas/MaricelNavarroSagarra/Game.java
@@ -0,0 +1,14 @@
+package cat.tecnocampus.mobileapps.practica2.LluisBoschNadal.IvanCarolNavas.MaricelNavarroSagarra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/cat/tecnocampus/mobileapps/practica2/LluisBoschNadal/IvanCarolNavas/MaricelNavarroSagarra/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/LluisBoschNadal/IvanCarolNavas/MaricelNavarroSagarra/MainActivity.java
@@ -1,8 +1,13 @@
 package cat.tecnocampus.mobileapps.practica2.LluisBoschNadal.IvanCarolNavas.MaricelNavarroSagarra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.ranking_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                Intent intent = new Intent(this, Ranking.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
