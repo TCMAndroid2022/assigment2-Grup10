fde03492864b3da8e35a64ef68b0c6a37a6bc61d
Maricel <59828070+Mar-i-cel@users.noreply.github.com>
2022-05-26 13:00:45 +0200

    commit 9

--- app/src/main/java/cat/tecnocampus/mobileapps/practica2/LluisBoschNadal/IvanCarolNavas/MaricelNavarroSagarra/PlayerDao.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/LluisBoschNadal/IvanCarolNavas/MaricelNavarroSagarra/PlayerDao.java
@@ -1,2 +1,22 @@
-package cat.tecnocampus.mobileapps.practica2.LluisBoschNadal.IvanCarolNavas.MaricelNavarroSagarra;public interface PlayerDao {
+package cat.tecnocampus.mobileapps.practica2.LluisBoschNadal.IvanCarolNavas.MaricelNavarroSagarra;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface PlayerDao {
+    @Query("SELECT * FROM player")
+    List<Player> getAll();
+
+    @Query("SELECT * FROM player WHERE uid IN (:playerIds)")
+    List<Player> loadAllByIds(int[] playerIds);
+
+    @Insert
+    void insert(Player player);
+
+    @Delete
+    void delete(Player player);
 }
